a254dc3c095fe439a71bdcf337879b96d207c35a
Ronald Tejada <ronald.tejada@oportunidades.org.sv>
2019-09-09 10:39:57 -0600

    API Rest done for 1 Client Table

---
+++ ApiRest_1/ApiRest_1/Controllers/ClientesEntityController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiRest_1.Models;
+
+namespace ApiRest_1.Controllers
+{
+    public class ClientesEntityController : ApiController
+    {
+        private Model1 db = new Model1();
+
+        // GET: api/ClientesEntity
+        public IQueryable<Cliente> GetCliente()
+        {
+            return db.Cliente;
+        }
+
+        // GET: api/ClientesEntity/5
+        [ResponseType(typeof(Cliente))]
+        public IHttpActionResult GetCliente(int id)
+        {
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cliente);
+        }
+
+        // PUT: api/ClientesEntity/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCliente(int id, Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cliente.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cliente).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ClientesEntity
+        [ResponseType(typeof(Cliente))]
+        public IHttpActionResult PostCliente(Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Cliente.Add(cliente);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = cliente.id }, cliente);
+        }
+
+        // DELETE: api/ClientesEntity/5
+        [ResponseType(typeof(Cliente))]
+        public IHttpActionResult DeleteCliente(int id)
+        {
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            db.Cliente.Remove(cliente);
+            db.SaveChanges();
+
+            return Ok(cliente);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClienteExists(int id)
+        {
+            return db.Cliente.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ ApiRest_1/ApiRest_1/Controllers/ClientesV3EntityController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Routing;
+using ApiRest_1.Models;
+
+namespace ApiRest_1.Controllers
+{
+    /*
+    Puede que la clase WebApiConfig requiera cambios adicionales para agregar una ruta para este controlador. Combine estas instrucciones en el método Register de la clase WebApiConfig según corresponda. Tenga en cuenta que las direcciones URL de OData distinguen mayúsculas de minúsculas.
+
+    using System.Web.Http.OData.Builder;
+    using System.Web.Http.OData.Extensions;
+    using ApiRest_1.Models;
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Cliente>("ClientesV3Entity");
+    config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+    */
+    public class ClientesV3EntityController : ODataController
+    {
+        private Model1 db = new Model1();
+
+        // GET: odata/ClientesV3Entity
+        [EnableQuery]
+        public IQueryable<Cliente> GetClientesV3Entity()
+        {
+            return db.Cliente;
+        }
+
+        // GET: odata/ClientesV3Entity(5)
+        [EnableQuery]
+        public SingleResult<Cliente> GetCliente([FromODataUri] int key)
+        {
+            return SingleResult.Create(db.Cliente.Where(cliente => cliente.id == key));
+        }
+
+        // PUT: odata/ClientesV3Entity(5)
+        public IHttpActionResult Put([FromODataUri] int key, Delta<Cliente> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Cliente cliente = db.Cliente.Find(key);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(cliente);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(cliente);
+        }
+
+        // POST: odata/ClientesV3Entity
+        public IHttpActionResult Post(Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Cliente.Add(cliente);
+            db.SaveChanges();
+
+            return Created(cliente);
+        }
+
+        // PATCH: odata/ClientesV3Entity(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public IHttpActionResult Patch([FromODataUri] int key, Delta<Cliente> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Cliente cliente = db.Cliente.Find(key);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(cliente);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(cliente);
+        }
+
+        // DELETE: odata/ClientesV3Entity(5)
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            Cliente cliente = db.Cliente.Find(key);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            db.Cliente.Remove(cliente);
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClienteExists(int key)
+        {
+            return db.Cliente.Count(e => e.id == key) > 0;
+        }
+    }
+}
